4c9955d6434f06065ccebae30832fbe00eeb84c1
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2023-01-28 00:27:52 -0300

    create project

---
+++ src/main/java/food/delivery/api/FoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package food.delivery.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryApplication.class, args);
+	}
+
+}
