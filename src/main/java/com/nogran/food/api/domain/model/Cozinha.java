70631cf8273f249140a387ee311e5a8846a39771
Mateus Nogueira <mateus.g.nogueira@hotmail.com>
2023-02-27 14:35:22 -0300

    3.15. Conhecendo e usando o Lombok

--- src/main/java/com/nogran/food/api/domain/model/Cozinha.java
+++ src/main/java/com/nogran/food/api/domain/model/Cozinha.java
@@ -2,29 +2,16 @@ package com.nogran.food.api.domain.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import java.util.Objects;
-
-@Entity
-@Table
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Cozinha {
+
+    @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nome;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Cozinha cozinha = (Cozinha) o;
-        return id.equals(cozinha.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
--- src/main/java/com/nogran/food/api/domain/model/Restaurante.java
+++ src/main/java/com/nogran/food/api/domain/model/Restaurante.java
@@ -3,16 +3,14 @@ package com.nogran.food.api.domain.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.util.Objects;
 
-@Entity
-@Table
 @Data
+@Entity
 public class Restaurante {
+
     @Id
     private Long id;
 
@@ -20,17 +18,4 @@ public class Restaurante {
 
     @Column(name = "delivery_tax")
     private BigDecimal deliveryTax;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Restaurante that = (Restaurante) o;
-        return id.equals(that.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
 }
