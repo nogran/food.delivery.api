a2185f2c0ba312228a31404f5b4582dd96751d19
Mateus Nogueira <mateus.g.nogueira@hotmail.com>
2023-07-27 16:51:50 -0300

    adding estado controller and rename method

---
+++ src/main/java/com/nogran/food/api/controller/EstadoController.java
@@ -0,0 +1,19 @@
+package com.nogran.food.api.controller;
+
+import com.nogran.food.api.domain.model.Estado;
+import com.nogran.food.api.domain.repository.EstadoRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/estados")
+public class EstadoController {
+
+    private EstadoRepository estadoRepository;
+
+    public List<Estado> listar(){
+        return estadoRepository.listar();
+    }
+
+}
--- src/main/java/com/nogran/food/api/domain/repository/EstadoRepository.java
+++ src/main/java/com/nogran/food/api/domain/repository/EstadoRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface EstadoRepository {
 
-    List<Estado> todos();
+    List<Estado> listar();
 
     Estado porId(Long id);
 
