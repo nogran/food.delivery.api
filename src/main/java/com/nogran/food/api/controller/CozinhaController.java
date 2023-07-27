8976a19533d31779e9284a54d6849eb06a71d81b
Mateus Nogueira <mateus.g.nogueira@hotmail.com>
2023-07-27 16:39:12 -0300

    adding controller and rename method

---
+++ src/main/java/com/nogran/food/api/controller/CozinhaController.java
@@ -0,0 +1,22 @@
+package com.nogran.food.api.controller;
+
+import com.nogran.food.api.domain.model.Cozinha;
+import com.nogran.food.api.domain.repository.CozinhaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
