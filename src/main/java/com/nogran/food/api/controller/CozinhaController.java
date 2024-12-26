0acba727303e1b541b1504be0cde94f0fc71f4d5
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2024-12-26 19:28:01 -0300

    adding put cuisine

--- src/main/java/com/nogran/food/api/controller/CozinhaController.java
+++ src/main/java/com/nogran/food/api/controller/CozinhaController.java
@@ -3,18 +3,14 @@ package com.nogran.food.api.controller;
 import com.nogran.food.api.model.CozinhasJacksonXmlElementWrapper;
 import com.nogran.food.domain.model.Cozinha;
 import com.nogran.food.domain.repository.CozinhaRepository;
-import java.util.List;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequestMapping("/cozinhas")
 @RestController
@@ -44,8 +40,19 @@ public class CozinhaController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cozinha adicionar(@RequestBody Cozinha cozinha){
+    public Cozinha adicionar(@RequestBody Cozinha cozinha) {
         return cozinhaRepository.salvar(cozinha);
+    }
+
+    @PutMapping("/{id}")
+    private ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+        Cozinha cozinhaAtual = cozinhaRepository.porId(id);
+        if (cozinhaAtual != null){
+            BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+            cozinhaAtual = cozinhaRepository.salvar(cozinhaAtual);
+            return ResponseEntity.ok(cozinhaAtual);
+        }
+        return ResponseEntity.notFound().build();
     }
 
 }
