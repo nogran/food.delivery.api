010666da9618e13124f3f2f27856d08fa5eb4ac9
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2024-12-27 18:54:31 -0300

    impl restaurant put method

--- src/main/java/com/nogran/food/api/controller/RestauranteController.java
+++ src/main/java/com/nogran/food/api/controller/RestauranteController.java
@@ -3,6 +3,7 @@ package com.nogran.food.api.controller;
 import com.nogran.food.domain.exception.EntidadeNaoEncontradaException;
 import com.nogran.food.domain.model.Restaurante;
 import com.nogran.food.domain.service.RestauranteService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -37,6 +38,24 @@ public class RestauranteController {
             restaurante = restauranteService.salvar(restaurante);
             return ResponseEntity.status(HttpStatus.CREATED)
                     .body(restaurante);
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.badRequest()
+                    .body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{restauranteId}")
+    public ResponseEntity<?> atualizar(@PathVariable Long restauranteId,
+                                       @RequestBody Restaurante restaurante) {
+        try {
+            Restaurante restauranteAtual = restauranteService.buscarPorId(restauranteId);
+            if (restauranteAtual != null) {
+                BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+                restauranteAtual = restauranteService.salvar(restauranteAtual);
+                return ResponseEntity.ok(restauranteAtual);
+            }
+            return ResponseEntity.notFound().build();
         } catch (EntidadeNaoEncontradaException e) {
             return ResponseEntity.badRequest()
                     .body(e.getMessage());
